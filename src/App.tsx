45bad4a1c0cba5f2ef7d9f1c39fabe980a26d375
sososim <armando29@live.com>
2022-01-28 11:46:29 -0500

    Hide number when is below or equall to 0

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,8 @@ function App() {
     <div>Output: </div>
       {
         [Desglosenumber(amount).map((e,i) =>
-          <div>{e} x {[2000,1000,500,200,100,50,25,10,5,1][i]} = {e * [2000,1000,500,200,100,50,25,10,5,1][i]}</div>
+          e <= 0 ? [] : 
+          <div>{e} x {[2000,1000,500,200,100,50,25,10,5,1][i]} = {e * [2000,1000,500,200,100,50,25,10,5,1][i]}</div> 
         ),
         <span className="error">{exceptionNumberisnotreal(amount)}</span>
         ]
