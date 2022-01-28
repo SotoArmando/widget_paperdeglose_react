ee96fae00ea8ddee06f4dfd7e907a06de545407c
sososim <armando29@live.com>
2022-01-27 19:41:32 -0500

    Added state representation using text

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import Desglosenumber from './Brain';
 function App() {
   const [numberElements, setNumberElements] = useState<number>(16850);
   const [exceptionNumberisnotreal] = [(e:number) => (e < 0 ? 'Number is not real' : '')] 
-  const [randomOutput, setOuput] = useState<Array<number>>(Desglosenumber(numberElements));
   
   const handleSequencyUpdate = function (e: React.FormEvent<HTMLInputElement>): void {
     setNumberElements(parseInt(e.currentTarget.value));
+
   }
 
   return (
@@ -17,8 +17,8 @@ function App() {
     <input value={numberElements} type="number" onChange={handleSequencyUpdate} />
     <div>Output: </div>
       {
-        [randomOutput.map(e =>
-          <div>{e}</div>
+        [Desglosenumber(numberElements).map((e,i) =>
+          <div>{e} x {[2000,1000,500,200,100,50,25,10,5,1][i]} = {e * [2000,1000,500,200,100,50,25,10,5,1][i]}</div>
         ),
         <span className="error">{exceptionNumberisnotreal(numberElements)}</span>
         ]
